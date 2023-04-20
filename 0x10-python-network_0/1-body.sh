ec4e3758e3975d9c09f1eb905c4bf1ac26ae8d0b
audusunday <108620954+audusunday@users.noreply.github.com>
2023-04-20 11:23:01 +0100

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/1-body.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Display only body of 200 status code response using curl
-curl -sL "$1"
--- 0x10-python-network_0/100-status_code.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# display a status code
-curl -s -o /dev/null -w "%{http_code}" "$1"
